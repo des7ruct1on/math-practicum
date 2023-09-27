368e9e0d4978f6d9c941fcfd846be4084fc72339
shad0w777 <shad0w2020@mail.ru>
2023-09-27 19:55:23 +0300

    1.5

--- 1/1.5/headers/lab.h
+++ 1/1.5/headers/lab.h
@@ -9,5 +9,22 @@
 #include <string.h>
 #include <ctype.h>
 
+typedef enum status_code {
+    code_succes,
+    code_overflow,
+    code_invalid_parameter,
+    code_diverge
+} status_code;
+
+bool compare_double_less(double a, double b, double epsilon)
+bool compare_double_bigger(double a, double b, double epsilon);
+bool compare_double_equal(double a, double b, double epsilon);
+status_code factorial(int num, double* res, double epsilon);
+status_code double_factorial(int num, double* res, double epsilon);
+status_code binary_power(double x, int n, double* res);
+status_code summary_a(double epsilon, double x, double* result);
+status_code summary_b(double epsilon, double x, double* result);
+status_code summary_c(double epsilon, double x, double* result);
+status_code summary_d(double epsilon, double x, double* result);
 
 #endif
